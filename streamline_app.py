67cd5c20fdb495842175c794eb6f276d649ea17d
gnadimut <128255876+gnadimut@users.noreply.github.com>
2023-03-18 15:49:33 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+sstreamlit.header( 'Breakfast Favorites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal') 
+streamlit.text ('© Kale, Spinach & Rocket Smoothie') 
+streamlit.text ('< Hard-Boiled Free-Range Egg')
+streamlit.text('SO Avocado Toast')
                
