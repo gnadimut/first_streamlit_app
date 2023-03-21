c84627d3ac674f7e6e7108442bf5359da5f0ddf4
gnadimut <128255876+gnadimut@users.noreply.github.com>
2023-03-21 11:02:30 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -35,7 +35,6 @@ try:
     streamlit.dataframe (back_from_function)
 except URLError as e:
     streamlit.error()
-streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
