c3000f89861638c5be26b2fe582dcdb247ce0a2c
gnadimut <128255876+gnadimut@users.noreply.github.com>
2023-03-21 11:23:53 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -55,7 +55,7 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding " + new_fruit
   
 if streamlit.button( 'Get Fruit List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"1)
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake")
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe (my_data_rows)
