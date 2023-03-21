cb2871d9ab3159c96daad7b6a085d6205d32f7c3
gnadimut <128255876+gnadimut@users.noreply.github.com>
2023-03-21 09:36:00 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe (fruits_to_show)
 streamlit.header ('Fruityvice Fruit Advice!')
 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 try:
@@ -31,8 +31,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice data(fruit_choice)
-    streamlit.dataframe (back from function)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe (back_from_function)
 except URLError as e:
     streamlit.error()
 streamlit.stop()
